c2116fe8a8fc8fbbbf607d7916cd8a23b003879e
Emre Demir <emre2548@gmail.com>
2020-09-09 22:15:47 +0300

    Add project files.

---
+++ Depo Takip/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Depo_Takip
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.tbItemNumAdd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbItemNameAdd = new System.Windows.Forms.ComboBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.cmbItemNameReduce = new System.Windows.Forms.ComboBox();
+            this.tbItemNumReduce = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnReduce = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ürün Adı:";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnAdd.Location = new System.Drawing.Point(232, 12);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 41);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Ekle";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // tbItemNumAdd
+            // 
+            this.tbItemNumAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tbItemNumAdd.Location = new System.Drawing.Point(93, 34);
+            this.tbItemNumAdd.Name = "tbItemNumAdd";
+            this.tbItemNumAdd.Size = new System.Drawing.Size(121, 22);
+            this.tbItemNumAdd.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(12, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ürün Adeti:";
+            // 
+            // cmbItemNameAdd
+            // 
+            this.cmbItemNameAdd.FormattingEnabled = true;
+            this.cmbItemNameAdd.Location = new System.Drawing.Point(93, 4);
+            this.cmbItemNameAdd.Name = "cmbItemNameAdd";
+            this.cmbItemNameAdd.Size = new System.Drawing.Size(121, 21);
+            this.cmbItemNameAdd.TabIndex = 7;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 108);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(963, 437);
+            this.listView1.TabIndex = 8;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "ADI";
+            this.columnHeader1.Width = 150;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "KALAN ADET";
+            this.columnHeader2.Width = 105;
+            // 
+            // cmbItemNameReduce
+            // 
+            this.cmbItemNameReduce.FormattingEnabled = true;
+            this.cmbItemNameReduce.Location = new System.Drawing.Point(421, 4);
+            this.cmbItemNameReduce.Name = "cmbItemNameReduce";
+            this.cmbItemNameReduce.Size = new System.Drawing.Size(121, 21);
+            this.cmbItemNameReduce.TabIndex = 13;
+            // 
+            // tbItemNumReduce
+            // 
+            this.tbItemNumReduce.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tbItemNumReduce.Location = new System.Drawing.Point(421, 34);
+            this.tbItemNumReduce.Name = "tbItemNumReduce";
+            this.tbItemNumReduce.Size = new System.Drawing.Size(121, 22);
+            this.tbItemNumReduce.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(340, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Ürün Adeti:";
+            // 
+            // btnReduce
+            // 
+            this.btnReduce.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnReduce.Location = new System.Drawing.Point(560, 12);
+            this.btnReduce.Name = "btnReduce";
+            this.btnReduce.Size = new System.Drawing.Size(75, 41);
+            this.btnReduce.TabIndex = 10;
+            this.btnReduce.Text = "Çıkart";
+            this.btnReduce.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(340, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Ürün Adı:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(987, 557);
+            this.Controls.Add(this.cmbItemNameReduce);
+            this.Controls.Add(this.tbItemNumReduce);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnReduce);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.cmbItemNameAdd);
+            this.Controls.Add(this.tbItemNumAdd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox tbItemNumAdd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbItemNameAdd;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ComboBox cmbItemNameReduce;
+        private System.Windows.Forms.TextBox tbItemNumReduce;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnReduce;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Depo Takip/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Depo_Takip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            ListViewItem listViewItemAdd = new ListViewItem();
+
+            listViewItemAdd.Text = cmbItemNameAdd.Text;
+            listViewItemAdd.SubItems.Add(tbItemNumAdd.Text);
+
+            listView1.Items.Add(listViewItemAdd);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbItemNameAdd.Items.Add("Kereste");
+            cmbItemNameAdd.Items.Add("Tornavida");
+            cmbItemNameAdd.Items.Add("Anahtar");
+            cmbItemNameAdd.Items.Add("Vida");
+            //cmbItemNameAdd.Items.Add("Poşet");
+
+            cmbItemNameReduce.Items.Add("Kereste");
+            cmbItemNameReduce.Items.Add("Tornavida");
+            cmbItemNameReduce.Items.Add("Anahtar");
+            cmbItemNameReduce.Items.Add("Vida");
+            //cmbItemNameReduce.Items.Add("Poşet");
+        }
+    }
+}
