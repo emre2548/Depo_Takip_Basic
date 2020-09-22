ef1f5c3caf94b0d930229e155711ab727445b310
Emre Demir <emre2548@gmail.com>
2020-09-22 22:38:10 +0300

    added DB

--- Depo Takip/Form1.Designer.cs
+++ Depo Takip/Form1.Designer.cs
@@ -29,155 +29,137 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.btnAdd = new System.Windows.Forms.Button();
-            this.tbItemNumAdd = new System.Windows.Forms.TextBox();
+            this.tbUrunAdeti = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.cmbItemNameAdd = new System.Windows.Forms.ComboBox();
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.cmbItemNameReduce = new System.Windows.Forms.ComboBox();
-            this.tbItemNumReduce = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.btnReduce = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tbUrunAdi = new System.Windows.Forms.TextBox();
+            this.tbStokKodu = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUrunSil = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(36, 37);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(62, 16);
+            this.label1.Size = new System.Drawing.Size(77, 18);
             this.label1.TabIndex = 0;
             this.label1.Text = "Ürün Adı:";
             // 
-            // btnAdd
+            // tbUrunAdeti
             // 
-            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.btnAdd.Location = new System.Drawing.Point(232, 12);
-            this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(75, 41);
-            this.btnAdd.TabIndex = 2;
-            this.btnAdd.Text = "Ekle";
-            this.btnAdd.UseVisualStyleBackColor = true;
-            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
-            // 
-            // tbItemNumAdd
-            // 
-            this.tbItemNumAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.tbItemNumAdd.Location = new System.Drawing.Point(93, 34);
-            this.tbItemNumAdd.Name = "tbItemNumAdd";
-            this.tbItemNumAdd.Size = new System.Drawing.Size(121, 22);
-            this.tbItemNumAdd.TabIndex = 4;
+            this.tbUrunAdeti.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tbUrunAdeti.Location = new System.Drawing.Point(119, 67);
+            this.tbUrunAdeti.Name = "tbUrunAdeti";
+            this.tbUrunAdeti.Size = new System.Drawing.Size(74, 22);
+            this.tbUrunAdeti.TabIndex = 4;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label2.Location = new System.Drawing.Point(12, 37);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(25, 71);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(73, 16);
+            this.label2.Size = new System.Drawing.Size(91, 18);
             this.label2.TabIndex = 3;
             this.label2.Text = "Ürün Adeti:";
             // 
-            // cmbItemNameAdd
+            // dataGridView1
             // 
-            this.cmbItemNameAdd.FormattingEnabled = true;
-            this.cmbItemNameAdd.Location = new System.Drawing.Point(93, 4);
-            this.cmbItemNameAdd.Name = "cmbItemNameAdd";
-            this.cmbItemNameAdd.Size = new System.Drawing.Size(121, 21);
-            this.cmbItemNameAdd.TabIndex = 7;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(1, 229);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1212, 459);
+            this.dataGridView1.TabIndex = 14;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             // 
-            // listView1
+            // tbUrunAdi
             // 
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.columnHeader1,
-            this.columnHeader2});
-            this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(12, 108);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(963, 437);
-            this.listView1.TabIndex = 8;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
+            this.tbUrunAdi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tbUrunAdi.Location = new System.Drawing.Point(119, 36);
+            this.tbUrunAdi.Name = "tbUrunAdi";
+            this.tbUrunAdi.Size = new System.Drawing.Size(294, 22);
+            this.tbUrunAdi.TabIndex = 15;
             // 
-            // columnHeader1
+            // tbStokKodu
             // 
-            this.columnHeader1.Text = "ADI";
-            this.columnHeader1.Width = 150;
+            this.tbStokKodu.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tbStokKodu.Location = new System.Drawing.Point(119, 8);
+            this.tbStokKodu.Name = "tbStokKodu";
+            this.tbStokKodu.Size = new System.Drawing.Size(294, 22);
+            this.tbStokKodu.TabIndex = 17;
             // 
-            // columnHeader2
+            // label5
             // 
-            this.columnHeader2.Text = "KALAN ADET";
-            this.columnHeader2.Width = 105;
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(25, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 18);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Stok Kodu :";
             // 
-            // cmbItemNameReduce
+            // btnGuncelle
             // 
-            this.cmbItemNameReduce.FormattingEnabled = true;
-            this.cmbItemNameReduce.Location = new System.Drawing.Point(421, 4);
-            this.cmbItemNameReduce.Name = "cmbItemNameReduce";
-            this.cmbItemNameReduce.Size = new System.Drawing.Size(121, 21);
-            this.cmbItemNameReduce.TabIndex = 13;
+            this.btnGuncelle.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnGuncelle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGuncelle.Location = new System.Drawing.Point(28, 110);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(119, 49);
+            this.btnGuncelle.TabIndex = 18;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = false;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
             // 
-            // tbItemNumReduce
+            // btnAdd
             // 
-            this.tbItemNumReduce.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.tbItemNumReduce.Location = new System.Drawing.Point(421, 34);
-            this.tbItemNumReduce.Name = "tbItemNumReduce";
-            this.tbItemNumReduce.Size = new System.Drawing.Size(121, 22);
-            this.tbItemNumReduce.TabIndex = 12;
+            this.btnAdd.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnAdd.Location = new System.Drawing.Point(170, 110);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(127, 49);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Yeni Ürün Ekle";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
-            // label3
+            // btnUrunSil
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label3.Location = new System.Drawing.Point(340, 37);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(73, 16);
-            this.label3.TabIndex = 11;
-            this.label3.Text = "Ürün Adeti:";
-            // 
-            // btnReduce
-            // 
-            this.btnReduce.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.btnReduce.Location = new System.Drawing.Point(560, 12);
-            this.btnReduce.Name = "btnReduce";
-            this.btnReduce.Size = new System.Drawing.Size(75, 41);
-            this.btnReduce.TabIndex = 10;
-            this.btnReduce.Text = "Çıkart";
-            this.btnReduce.UseVisualStyleBackColor = true;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label4.Location = new System.Drawing.Point(340, 9);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(62, 16);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "Ürün Adı:";
+            this.btnUrunSil.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnUrunSil.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnUrunSil.Location = new System.Drawing.Point(312, 110);
+            this.btnUrunSil.Name = "btnUrunSil";
+            this.btnUrunSil.Size = new System.Drawing.Size(140, 49);
+            this.btnUrunSil.TabIndex = 10;
+            this.btnUrunSil.Text = "Ürün Sil";
+            this.btnUrunSil.UseVisualStyleBackColor = false;
+            this.btnUrunSil.Click += new System.EventHandler(this.btnDepoCikisi_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(987, 557);
-            this.Controls.Add(this.cmbItemNameReduce);
-            this.Controls.Add(this.tbItemNumReduce);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.btnReduce);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.listView1);
-            this.Controls.Add(this.cmbItemNameAdd);
-            this.Controls.Add(this.tbItemNumAdd);
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(1214, 684);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.tbStokKodu);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbUrunAdi);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnUrunSil);
+            this.Controls.Add(this.tbUrunAdeti);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -186,18 +168,15 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button btnAdd;
-        private System.Windows.Forms.TextBox tbItemNumAdd;
+        private System.Windows.Forms.TextBox tbUrunAdeti;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ComboBox cmbItemNameAdd;
-        private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.ColumnHeader columnHeader1;
-        private System.Windows.Forms.ColumnHeader columnHeader2;
-        private System.Windows.Forms.ComboBox cmbItemNameReduce;
-        private System.Windows.Forms.TextBox tbItemNumReduce;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button btnReduce;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox tbUrunAdi;
+        private System.Windows.Forms.TextBox tbStokKodu;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUrunSil;
     }
 }
 
--- Depo Takip/Form1.cs
+++ Depo Takip/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Depo_Takip
 {
@@ -16,30 +17,93 @@ namespace Depo_Takip
             InitializeComponent();
         }
 
+        SqlConnection connectDB = new SqlConnection(@"Data Source = ASUS;Initial Catalog=depotakip;Integrated Security=True");
+
         private void btnAdd_Click(object sender, EventArgs e)
         {
-            ListViewItem listViewItemAdd = new ListViewItem();
+            connectDB.Open();
+            SqlCommand ekle = new SqlCommand("insert into UrunBilgisi(StokKodu,UrunAdi,Adet) values(@p1,@p2,@p3)",connectDB);
+            ekle.Parameters.AddWithValue("@p1",tbStokKodu.Text);
+            ekle.Parameters.AddWithValue("@p2",tbUrunAdi.Text);
+            ekle.Parameters.AddWithValue("@p3",tbUrunAdeti.Text);
 
-            listViewItemAdd.Text = cmbItemNameAdd.Text;
-            listViewItemAdd.SubItems.Add(tbItemNumAdd.Text);
+            ekle.ExecuteNonQuery();
 
-            listView1.Items.Add(listViewItemAdd);
+            connectDB.Close();
 
+
+            Listele();
         }
+
+        
+
+
+
+        private void Listele()
+        {
+            SqlDataAdapter sqlData = new SqlDataAdapter("select * from UrunBilgisi",connectDB);
+            DataSet dataset = new DataSet();
+            sqlData.Fill(dataset);
+            dataGridView1.DataSource = dataset.Tables[0];
+        }
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            cmbItemNameAdd.Items.Add("Kereste");
-            cmbItemNameAdd.Items.Add("Tornavida");
-            cmbItemNameAdd.Items.Add("Anahtar");
-            cmbItemNameAdd.Items.Add("Vida");
-            cmbItemNameAdd.Items.Add("Poşet");
+            Listele();
+        }
 
-            cmbItemNameReduce.Items.Add("Kereste");
-            cmbItemNameReduce.Items.Add("Tornavida");
-            cmbItemNameReduce.Items.Add("Anahtar");
-            cmbItemNameReduce.Items.Add("Vida");
-            cmbItemNameReduce.Items.Add("Poşet");
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            connectDB.Open();
+            SqlCommand command = new SqlCommand("select Adet from UrunBilgisi where StokKodu=@p1", connectDB);
+            command.Parameters.AddWithValue("@p1", tbStokKodu.Text);
+            SqlDataReader urunAdet = command.ExecuteReader();
+
+            int adet = 0;
+
+            while (urunAdet.Read())
+            {
+                adet = Convert.ToInt32(urunAdet[0]);
+            }
+            connectDB.Close();
+
+            connectDB.Open();
+            SqlCommand guncelle = new SqlCommand("update UrunBilgisi set StokKodu=@p1,UrunAdi=@p2,Adet=@p3 where StokKodu=@p1", connectDB);
+            guncelle.Parameters.AddWithValue("@p1", tbStokKodu.Text);
+            guncelle.Parameters.AddWithValue("@p2", tbUrunAdi.Text);
+
+            int yeniUrunAdet = Convert.ToInt32(adet) + Convert.ToInt32(tbUrunAdeti.Text);
+
+            guncelle.Parameters.AddWithValue("@p3", yeniUrunAdet.ToString());
+            guncelle.ExecuteNonQuery();
+            connectDB.Close();
+
+            Listele();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int sec = dataGridView1.SelectedCells[0].RowIndex;
+
+            string StokKodu = dataGridView1.Rows[sec].Cells[0].Value.ToString();
+            string UrunAdi = dataGridView1.Rows[sec].Cells[1].Value.ToString();
+            string Adet = dataGridView1.Rows[sec].Cells[2].Value.ToString();
+
+            tbStokKodu.Text = StokKodu;
+            tbUrunAdi.Text = UrunAdi;
+            tbUrunAdeti.Text = Adet;
+
+        }
+
+        private void btnDepoCikisi_Click(object sender, EventArgs e)
+        {
+            connectDB.Open();
+            SqlCommand command = new SqlCommand("Delete from UrunBilgisi where StokKodu='@p1'", connectDB);
+            command.Parameters.AddWithValue("@p1", tbStokKodu.Text);
+            command.ExecuteNonQuery();
+            connectDB.Close();
+
         }
     }
 }
